c185c6e1865d052267b5b8ae375d27578aff5c61
Manraj Thind <thindm@uoguelph.ca>
2025-10-01 11:58:57 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,5 +3,21 @@
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
+user_input = input("User input: ")
+
+if user_input.isadigit(){
+  m = int(user_input)
+# Validate that the input is a positive integer
+  if m > 0:
+    a = 0
+    b = 1
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+    for i in range(m):
+      print(a, end = "Expected Outcome: ")
+      a, b = b, a + b
+  else:
+      print("Please enter a positive integer.")
+else:
+      print("Please enter a positive integer.")
+      
+      
